5b3ef2dcc84a7924069d8bb0aa1f3c54f7333caf
Cauã Apicelo <developer.caua@gmail.com>
2023-11-18 18:51:44 -0300

    :tada: initial commit: curso de lógica com js na Alura

---
+++ curso-alura/curso-logica-funcao-lista-alura/curso-logica-js-alura-2/app.js
@@ -0,0 +1,37 @@
+
+
+// Manipulando Texto com JS
+// let titulo = document.querySelector('h1'); 
+// titulo.innerHTML = "Jogo do Número Secreto" ;
+
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+// let button = document.querySelector('#teste'); 
+// button.innerHTML = "Novo botão, kkkkkkkkkkkkk";
+
+// Criando função
+
+// function verificarChute(params) {
+
+// }
+
+let numeroSecreto = gerarNumeroAleatorioi();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute(params) {
+    let chute = document.querySelector('input').value; //pegar o valor do input
+    console.log(chute ==numeroSecreto);
+}
+
+function gerarNumeroAleatorioi(params) {
+    return parseInt(Math.random() * 10 + 1);
+    
+}
