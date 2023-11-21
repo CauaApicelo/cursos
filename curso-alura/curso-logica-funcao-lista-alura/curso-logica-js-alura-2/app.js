9e439e396556e34fd1c7669de4b577e5e86d1874
Cauã Apicelo <developer.caua@gmail.com>
2023-11-21 14:19:34 -0300

    :sparkles: feat(funções e listas): curso finalizado

--- curso-alura/curso-logica-funcao-lista-alura/curso-logica-js-alura-2/app.js
+++ curso-alura/curso-logica-funcao-lista-alura/curso-logica-js-alura-2/app.js
@@ -1,37 +1,85 @@
+// Lista de números sorteados
+let listaDeNumerosSorteados = [];
+
+let numeroLimite = 10;
+
+// Variável que armazena o número secreto 
+let numeroSecreto = gerarNumeroAleatorio();
+
+// Variável das tentivas que se inicia com 1
+let tentivas = 1;
 
 
-// Manipulando Texto com JS
-// let titulo = document.querySelector('h1'); 
-// titulo.innerHTML = "Jogo do Número Secreto" ;
 
-// let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = "Escolha um número entre 1 e 10";
-
-// let button = document.querySelector('#teste'); 
-// button.innerHTML = "Novo botão, kkkkkkkkkkkkk";
-
-// Criando função
-
-// function verificarChute(params) {
-
-// }
-
-let numeroSecreto = gerarNumeroAleatorioi();
-
+// Função que manipula uma tag e muda o texto
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial(params) {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
 
+exibirMensagemInicial()
+
+// Função que verifica o valor posto no input e fazer as condições
 function verificarChute(params) {
     let chute = document.querySelector('input').value; //pegar o valor do input
     console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!'); //Utilizei a função exibirTextoNaTela para exibir no h1 a mensagem "Acertou!"
+        // Se tentativa for maior que 1, então 'tentativas', se não 'tentativa'.
+        let palavraTentativa = tentivas > 1 ? 'tentativas' : 'tentativa';
+
+        let mensagemTentativas = `Você acertou o número secreto com ${tentivas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+
+        // Desabilitando o disabled do button reiniciar 
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentivas++;
+        limparCampo();
+    }
 }
 
-function gerarNumeroAleatorioi(params) {
-    return parseInt(Math.random() * 10 + 1);
-    
+// Função que gera o número aleatório de forma inteira 
+function gerarNumeroAleatorio(params) {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadedeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadedeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados == [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
 }
+
+// Função para limpar o campo
+function limparCampo(params) {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(params) {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentivas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+
+// length = tamanho da lista 
---
+++ curso-alura/curso-logica-funcao-lista-alura/curso-logica-js-alura-desafio-3/app.js
@@ -0,0 +1,18 @@
+// Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+function calcularIMC(altura, peso) {
+    let imc = peso / (altura * altura);
+    return imc;
+}
+
+console.log(calculaIMC(1.58, 65));
+
+// Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+function calcularFatorial() {
+    let fatorial = n*(n)
+}
+
+// Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+function converterDolarParaReal() {
+    let cotacaoDoDolar = 4.80;
+
+}
